61d570355b0e0b3a5448b01e2c1a299035100f89
sugirru <sugirru@gmail.com>
2022-12-26 15:21:13 +0800

    add roll command

---
+++ commands/roll.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roll')
+        .setDescription('Roll a random number within range specified (default 6)')
+        .addIntegerOption(option =>
+            option
+                .setName('range')
+                .setDescription('Random number will be within 0 to specified range')
+                .setMinValue(1)),
+    async execute(interaction) {
+        const min = 1;
+        const max = interaction.options.getInteger('range') ?? 6;
+
+        const rolledNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+        await interaction.reply('**' + rolledNumber + '**');
+    },
+};
