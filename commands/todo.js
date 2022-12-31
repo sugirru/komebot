544e7902285873d96b4c2cba6305fd34a747e8e3
sugirru <sugirru@gmail.com>
2022-12-31 15:19:55 +0800

    update todo listing

--- commands/todo.js
+++ commands/todo.js
@@ -3,81 +3,41 @@ const { SlashCommandBuilder, ActionRowBuilder, EmbedBuilder, ButtonBuilder, Butt
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('todo')
-        .setDescription('Todo manager')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('options')
-                .setDescription('See possible actions'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('list')
-                .setDescription('List existing todos')),
+        .setDescription('Todo manager'),
     async execute(interaction) {
+        // GET TODOS DATABASE
         const Todos = interaction.client.todos;
 
-        if (interaction.options._subcommand === 'options') {
-            const Buttons = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('newTodoButton')
-                        .setLabel('New Todo')
-                        .setStyle(ButtonStyle.Primary),
-                );
+        // CREATE BUTTONS BELOW EMBED
+        const Buttons = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('newTodoButton')
+                    .setLabel('New Todo')
+                    .setStyle(ButtonStyle.Primary),
+            );
 
-            const message = await interaction.reply({ components: [Buttons], ephemeral: true });
-            const collector = await message.createMessageComponentCollector();
-            setTimeout(() => interaction.deleteReply(), 10000);
-
-            collector.on('collect', async i => {
-                if (i.customId === 'newTodoButton') {
-                    // Create modal to be shown to user
-                    const modal = new ModalBuilder()
-                        .setCustomId('newTodoModal')
-                        .setTitle('New Todo');
-
-                    // Create inputs for the user
-                    const nameInput = new TextInputBuilder()
-                        .setCustomId('todoNameInput')
-                        .setLabel('Todo Name')
-                        .setStyle(TextInputStyle.Short);
-                    const descriptionInput = new TextInputBuilder()
-                        .setCustomId('todoDescriptionInput')
-                        .setLabel('Todo Description (Optional)')
-                        .setStyle(TextInputStyle.Short);
-
-                    // Create Action Rows for the inputs
-                    const firstActionRow = new ActionRowBuilder().addComponents(nameInput);
-                    const secondActionRow = new ActionRowBuilder().addComponents(descriptionInput);
-
-                    // Add all inputs to modal
-                    modal.addComponents(firstActionRow, secondActionRow);
-
-                    // Show the modal to the user
-                    await i.showModal(modal);
-
-                    // // 5 minute timeout for command
-                    // // Workaround as there is currently no way to determine if user cancelled modal
-                    // setTimeout(() => interaction.deleteReply(), 10000);
-                }
-            });
-        } else if (interaction.options._subcommand === 'list') {
-            const TodoList = await Todos.findAll({ where: { userid: interaction.user.id } });
+        // START CREATING EMBEDS
+        const TodoList = await Todos.findAll({ where: { userid: interaction.user.id } });
+        const embedArray = [];
+        if (TodoList.length != 0) {
             const userTodos = eval(JSON.stringify(TodoList));
-
             console.log(userTodos); // DEBUG
 
-            const embedArray = [];
             let pageTodos = [];
             let page = 1;
-            // let todoIndex = 0;
-            // let pageTodoAmount = 0;
+            let pageString = '';
+            let todoNumber = 1;
+            let todosOnPage = 0;
+
+            // todo is a number in loop
             for (const todo in userTodos) {
-                console.log(todo); // DEBUG
+                console.log('here one'); // DEBUG
                 if (pageTodos.length < 4) {
                     pageTodos.push(userTodos[todo]);
+                    todosOnPage += 1;
                 } else {
-                    let pageString = '';
-                    let todoNumber = 1;
+
                     for (const pageTodo in pageTodos) {
                         pageString = pageString.concat(todoNumber.toString() + '. '); // Todo Number
                         pageString = pageString.concat(pageTodos[pageTodo].name + '\n\t'); // Todo Name
@@ -93,56 +53,77 @@ module.exports = {
                             value: pageString,
                         }));
 
+                    pageString = '';
+                    todoNumber = 1;
+                    todosOnPage = 0;
+
                     pageTodos = [];
                     page += 1;
+
                 }
+
+
             }
-
-            // const Embed = new EmbedBuilder()
-            //     .setColor(0x0099FF)
-            //     .setTitle('Todos')
-            //     .addFields({ name: 'field', value: 'test' });
-
-            // const Row = new ActionRowBuilder()
-            //     .addComponents(
-            //         new ButtonBuilder()
-            //             .setCustomId('button')
-            //             .setLabel('Button')
-            //             .setStyle(ButtonStyle.Primary),
-            //     );
-
-            // await interaction.reply({ embeds: [Embed], components: [Row], ephemeral: true });
-            const message = await interaction.reply({ embeds: [embedArray[0]], ephemeral: true });
-            // const collector = await message.createMessageComponentCollector();
-            setTimeout(() => interaction.deleteReply(), 10000);
-
-            // await message.react('👍')
-            //     .then(() => message.react('👎'));
-
-            // const filter = (reaction, user) => {
-            //     return ['👍', '👎'].includes(reaction.emoji.name) && user.id === interaction.user.id;
-            // };
-
-            // await message.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
-            //     .then(collected => {
-            //         const reaction = collected.first();
-
-            //         if (reaction.emoji.name === '👍') {
-            //             message.reply('You reacted with a thumbs up.');
-            //         } else {
-            //             message.reply('You reacted with a thumbs down.');
-            //         }
-            //     })
-            //     .catch(collected => {
-            //         message.reply('You reacted with neither a thumbs up, nor a thumbs down.');
-            //     });
-
-            // collector.on('collect', async i => {
-            //     if (i.customId === 'button') {
-            //         await i.update('pressed button');
-            //     }
-            // });
+            // Create final embed if there are still todos remaining after previous loop
+            if (todosOnPage < 4) {
+                for (const pageTodo in pageTodos) {
+                    pageString = pageString.concat(todoNumber.toString() + '. '); // Todo Number
+                    pageString = pageString.concat(pageTodos[pageTodo].name + '\n\t'); // Todo Name
+                    pageString = pageString.concat(pageTodos[pageTodo].description + '\n'); // Todo Description
+                    todoNumber += 1;
+                }
+                embedArray.push(new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle('Todo List')
+                    .addFields({
+                        name: 'Page ' + page,
+                        value: pageString,
+                    }));
+            }
+        } else {
+            console.log('here two'); // DEBUG
+            embedArray.push(new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle('Todo List')
+                .addFields({
+                    name: 'EMPTY',
+                    value: 'No tasks yet!',
+                }));
         }
 
+        // SEND MESSAGE
+        const message = await interaction.reply({ embeds: [embedArray[0]], components: [Buttons], ephemeral: true });
+        const collector = await message.createMessageComponentCollector();
+        setTimeout(() => interaction.deleteReply(), 10000);
+
+        // COLLECTOR FOR BUTTONS COMPONENT IN MESSAGE
+        collector.on('collect', async i => {
+            if (i.customId === 'newTodoButton') {
+                // Create modal to be shown to user
+                const modal = new ModalBuilder()
+                    .setCustomId('newTodoModal')
+                    .setTitle('New Todo');
+
+                // Create inputs for the user
+                const nameInput = new TextInputBuilder()
+                    .setCustomId('todoNameInput')
+                    .setLabel('Todo Name')
+                    .setStyle(TextInputStyle.Short);
+                const descriptionInput = new TextInputBuilder()
+                    .setCustomId('todoDescriptionInput')
+                    .setLabel('Todo Description (Optional)')
+                    .setStyle(TextInputStyle.Short);
+
+                // Create Action Rows for the inputs
+                const firstActionRow = new ActionRowBuilder().addComponents(nameInput);
+                const secondActionRow = new ActionRowBuilder().addComponents(descriptionInput);
+
+                // Add all inputs to modal
+                modal.addComponents(firstActionRow, secondActionRow);
+
+                // Show the modal to the user
+                await i.showModal(modal);
+            }
+        });
     },
 };
